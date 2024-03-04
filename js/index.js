8bca1d9ae04146ece3f5ae70783b48be988b33bf
Emmanuel Umeh <emmachinemerem04@gmail.com>
2024-03-03 20:09:51 -0800

    navbar

--- js/index.js
+++ js/index.js
@@ -53,6 +53,35 @@
 //   prevScroll = currentScrollPos;
 // }
 
+function isInViewport(element) {
+  var rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+// Function to handle scroll events
+function handleScroll() {
+  var elements = document.querySelectorAll('.initial-state');
+  elements.forEach(function (element) {
+    if (isInViewport(element)) {
+      element.classList.add('animate__animated', 'animate__backInLeft');
+      element.classList.remove('initial-state');
+    }
+  });
+}
+
+// Attach the handleScroll function to the scroll event
+window.addEventListener('scroll', handleScroll);
+
+// Trigger the handleScroll function on page load
+window.addEventListener('load', handleScroll);
+
+
+
 function FooterDate() {
   const newDate = new Date();
   const year = newDate.getFullYear();
